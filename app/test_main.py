3b001170c393221e57438cf22bc79c81e947af63
Oleh <sholeg2005@gmail.com>
2024-11-27 00:16:32 +0100

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,19 @@
 from app.main import get_coin_combination
 
 
-# write your tests here
+def test_all_zero() -> None:
+    cents = 0
+    result = get_coin_combination(cents)
+    assert result == [0, 0, 0, 0]
+
+
+def test_50_cents() -> None:
+    cents = 50
+    result = get_coin_combination(cents)
+    assert result == [0, 0, 0, 2]
+
+
+def test_17_cents() -> None:
+    cents = 17
+    result = get_coin_combination(cents)
+    assert result == [2, 1, 1, 0]
